50d232606a113fbd41c46da370221ed1aa9e8ce8
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-06-06 09:11:48 -0400

    Add links to external program repos

--- centreseq/bin/tree/wrappers.py
+++ centreseq/bin/tree/wrappers.py
@@ -8,6 +8,8 @@ def call_snp_sites(aligned_multifasta: Path, outdir: Path) -> Path:
     Calls snp-sites on an aligned multiFASTA file and produces a VCF file as output.
     Will only generate an output file if variants are detected.
 
+    https://github.com/sanger-pathogens/snp-sites
+
     :param aligned_multifasta: Path to multi-FASTA containing alignment of a core gene
     :param outdir: Path to desired output directory
     :return: Path to VCF
@@ -21,6 +23,8 @@ def call_snp_sites(aligned_multifasta: Path, outdir: Path) -> Path:
 def call_muscle(infile: Path) -> Path:
     """
     Produces an aligned version of an input FASTA file (overwrites the original)
+
+    https://www.drive5.com/muscle/
     """
     cmd = f"muscle -in {infile} -out {infile} -maxiters 1"
     run_subprocess(cmd, get_stdout=True)
