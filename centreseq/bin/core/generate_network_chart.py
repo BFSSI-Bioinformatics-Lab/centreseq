7f109632b55c060c0d6efd8bd20310423311d4bf
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-07-05 10:25:12 -0400

    Support for the network coding file

--- centreseq/bin/core/generate_network_chart.py
+++ centreseq/bin/core/generate_network_chart.py
@@ -12,13 +12,14 @@ assert NETWORK_GRAPH_HTML_TEMPLATE.exists()
 # D3_V5 = '/graph_assets/d3.v5.min.js'
 # D3_SIMPLE_SLIDER = './graph_assets/d3-simple-slider.js'
 
-def generate_network_chart(pairwise_gene_count_report: Path, roary_report: Path, outdir: Path):
+def generate_network_chart(pairwise_gene_count_report: Path, roary_report: Path, network_coding: Path, outdir: Path):
     # Read in the file
     with open(NETWORK_GRAPH_HTML_TEMPLATE, 'r') as file:
         filedata = file.read()
 
     # Replace the target string
     filedata = filedata.replace('ROARY_GENE_COUNT_REPORT_VARIABLE', roary_report.name)
+    filedata = filedata.replace('NETWORK_CODING', network_coding.name)
     filedata = filedata.replace('PAIRWISE_GENE_COUNT_REPORT_TSV', pairwise_gene_count_report.name)
     filedata = filedata.replace('D3_V5', str(D3_V5))
     filedata = filedata.replace('D3_SIMPLE_SLIDER', str(D3_SIMPLE_SLIDER))
@@ -31,3 +32,14 @@ def generate_network_chart(pairwise_gene_count_report: Path, roary_report: Path,
         file.write(filedata)
 
     return outfile
+
+
+def generate_network_chart_coding_file(outdir: Path, sample_id_list: list) -> Path:
+    network_coding = outdir / 'network_graph_coding.tsv'
+    if network_coding.exists():
+        network_coding.unlink()
+    with open(str(network_coding), 'w') as file:
+        file.write(f"sample_id\tgroup_id\n")
+        for sample in sample_id_list:
+            file.write(f"{sample}\tdefault\n")
+    return network_coding
