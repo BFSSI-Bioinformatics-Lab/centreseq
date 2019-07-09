0c97bcbf4e0c0d1a318dda0ef8d368993ae39d44
bfssi-forest-dussault <forest.dussault@canada.ca>
2019-07-09 08:44:21 -0400

    0.3.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-__version__ = '0.2.3'
+__version__ = '0.3.0'
 __author__ = ['Forest Dussault', 'Adrian Verster', 'Nicholas Petronella']
 __email__ = 'forest.dussault@canada.ca'
 
